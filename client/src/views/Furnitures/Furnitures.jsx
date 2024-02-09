7821382be840dcc06119956248a7fdf486d10660
Robertoaclu <robertoaclu@gmail.com>
2024-02-09 20:38:39 +0100

    Furnitures fetch and component view

--- client/src/views/Furnitures/Furnitures.jsx
+++ client/src/views/Furnitures/Furnitures.jsx
@@ -1,3 +1,23 @@
+import { useState, useEffect } from "react";
+import { host } from "../../const/host";
+import FurnituresView from "./FurnituresView";
+
+
 export default function Furnitures() {
-  return <h1>Esto es Furnitures</h1>;
+  const [furnitures, setFurnitures] = useState(null);
+
+  useEffect(() => {
+    async function fetchFurnitures() {
+      try {
+        const response = await fetch(`${host}/mobiliario`);
+        const furnitures = await response.json();
+        setFurnitures(furnitures);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchFurnitures();
+  }, []);
+
+  return <FurnituresView clients={furnitures} />;
 }
