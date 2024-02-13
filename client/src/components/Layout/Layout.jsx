b7e152e9815f1eb8459d5fa99d82759cb7dbcae9
Davilesc <davileesc@gmail.com>
2024-02-13 16:56:21 +0100

    Tambien eliminamos la importacion de Toast

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import { Grid } from "@mui/material";
 import Footer from "../Footer/Footer";
-import { Toaster } from "react-hot-toast";
 
 export default function Layout() {
   return (
