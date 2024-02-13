07ea2d1b9b6c60b3bdc16dd11457de906a14f493
Luis Montesinos <luismontesinoslopez@gmail.com>
2024-02-13 20:30:22 +0100

    clientsDetail modified to coincide with the other views

--- client/src/components/BoxComponents/BoxComponents.jsx
+++ client/src/components/BoxComponents/BoxComponents.jsx
@@ -1,10 +1,10 @@
 import { Box, Typography } from "@mui/material";
 
-export default function BoxComponents({ label, Data }) {
+export default function BoxComponents({ label, data }) {
     return (
             <Box sx={{ border: "1px solid black", p: 1, textAlign: "center", borderRadius: "10px", width: "140px" }}>
                 <Typography sx={{ fontWeight: "bold" }}>{label}</Typography>
-                <Typography sx={{ p: 1 }}>{Data}</Typography>
+                <Typography sx={{ p: 1 }}>{data}</Typography>
             </Box>
     )
 }
--- client/src/views/EventDetails/EventDetails.jsx
+++ client/src/views/EventDetails/EventDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box, Button, Grid, Paper, TextField, Typography } from '@mui/material';
 import { host } from "../../const/host"
